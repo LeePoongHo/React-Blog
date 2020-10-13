37535f09db79e4f9e1df27b9ec0a30035a3ca87c
LeePoongHo <dlvndghs@naver.com>
2020-10-13 21:25:14 +0900

    set up server & client

--- react-blog/server/server.js
+++ react-blog/server/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send(`Welcome to React Blog`);
+// app.get: 서버 응답 출력
+app.get('/api/host', (req, res) => {
+    res.send({host: 'poongho'});
 })
 
+// 해당 포트의 서버 실행
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}`);
 })
